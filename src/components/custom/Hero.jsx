f75dc970f6a2de3e3fac0b8839aef8a09328f178
Mansi Pant <2022csmansi12020@poornima.edu.in>
2025-09-21 02:43:33 +0530

    update

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className='flex items-center flex-col m-16'>
       <h1 className='font-extrabold text-3xl text-center'>Plan Your <span className='text-[#FF5050] text-shadow-md'>Perfect Trip</span>  with AI-Powered Precision</h1>
-      <h2 className=' text-2xl text-center text-gray-700 m-13 w-[60%]'>Discover personalized itineraries, hidden gems, and stress-free travel planning — all tailored just for you by smart AI.</h2>
+      <h2 className=' text-2xl text-center text-gray-700 m-13 w-[60%]'>Unburden yourself from the noise of daily life and the hassle of planning. Our smart AI builds your perfect path to peace and adventure.</h2>
       <Link to="/plan-trip"><Button className=" cursor-pointer p-5">Plan Your Trip &gt; </Button></Link>
 
       <img className='m-20' src="/Landing.png" alt="" />
