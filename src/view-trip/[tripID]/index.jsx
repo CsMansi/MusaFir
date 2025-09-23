04276463100a99337e88d49d6e159b4a4a34f51d
Mansi Pant <2022csmansi12020@poornima.edu.in>
2025-09-24 01:08:52 +0530

    Update

--- src/view-trip/[tripID]/index.jsx
+++ src/view-trip/[tripID]/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { doc, getDoc } from 'firebase/firestore';
-import { db } from '@/service/firebaseConfig';
+import { db } from '@/service/firebaseConfig.jsx';
 import { useParams } from 'react-router-dom';
 import { toast } from 'sonner';
 
