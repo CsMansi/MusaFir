d93972a864ce81f9f96c0d09b341c2eea921236b
Mansi Pant <2022csmansi12020@poornima.edu.in>
2025-09-24 00:47:28 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@/App.css';
 import Navbar from '@/components/custom/Navbar.jsx';
-import Footer from '@/components/custom/Footer.jsx';
+import Footer from './components/custom/Footer.jsx';
 import { Toaster } from "@/components/ui/sonner";
 import { Outlet } from 'react-router-dom';
 
